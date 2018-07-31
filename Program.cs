77ae9572f1ddc9f2ce79a721bfc859a15ebb2627
teeyuseng <cowghostsnakegod@gmail.com>
2018-07-31 14:21:26 +0800

    text

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class P
+{
+    static ConsoleColor the_original_F = Console.ForegroundColor;
+    static void Main(string[] args)
+    {
+        int x = -86;
+        Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$");
+        Console.Write("原為 ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(x);
+        Console.ForegroundColor = the_original_F;
+        //  fix #1 觀察下面註解的語法
+        /* 讓 x 變數存個
+        0 或 1
+        的值 */
+        x = (int)(new Random().NextDouble() * 2);
+        Console.WriteLine("?????????????????????????");
+        Console.Write("後來為 ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine(x);
+        Console.ForegroundColor = the_original_F;
+        Console.WriteLine("#########################");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
